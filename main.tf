95d8744184b643f80fcee57efbaf1d9b5ba7c038
FKouhai <frandres00@gmail.com>
2024-05-10 03:13:12 +0200

    Included terraform stuff

--- main.tf
+++ main.tf
@@ -1,1 +1,37 @@
-# write some TF 
+resource "aws_ecs_cluster" "ctfd-cluser" {
+  name = var.ECS_CLUSTER
+}
+
+resource "aws_ecs_cluster_capacity_providers" "cap-providers" {
+  cluster_name       = aws_ecs_cluster.ctfd-cluser
+  capacity_providers = ["FARGATE_SPOT"]
+
+}
+
+resource "aws_ecs_task_definition" "ctfd-task" {
+  family                = var.SERVICE_NAME
+  container_definitions = file("./service.json")
+  network_mode          = "awsvpc"
+  cpu                   = 1024
+  memory                = 3072
+
+}
+
+resource "aws_ecs_service" "ctfd-svc" {
+  name            = var.SERVICE_NAME
+  cluster         = aws_ecs_cluster.ctfd-cluser.id
+  task_definition = aws_ecs_task_definition.ctfd-task.arn
+  desired_count   = 1
+  network_configuration {
+    subnets          = aws_subnet.main.id
+    security_groups  = aws_security_group.ctfd-secgroup.id
+    assign_public_ip = true
+  }
+
+}
+resource "aws_ecs_task_set" "ctfd-task-set" {
+  service         = aws_ecs_service.ctfd-svc.id
+  cluster         = aws_ecs_cluster.ctfd-cluser.id
+  task_definition = aws_ecs_task_definition.ctfd-task.arn
+
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "ECS_CLUSTER" {
+  type = string
+}
+variable "SERVICE_NAME" {
+  type = string
+}
+variable "CIDR" {
+  type = string
+}
+variable "SUBCIDR" {
+  type = string
+}
+variable "SECGROUP" {
+  type = string
+}
+variable "CTFD_PORT" {
+  type = number
+}
