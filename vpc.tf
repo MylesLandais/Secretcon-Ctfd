b3d00432c8af741eac3cd844f6f987462f99c058
FKouhai <frandres00@gmail.com>
2024-05-11 02:54:30 +0200

    Included routes to the internet

--- vpc.tf
+++ vpc.tf
@@ -64,3 +64,24 @@ resource "aws_internet_gateway" "default" {
   vpc_id = aws_vpc.main.id
 
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = var.az_count
+  subnet_id      = aws_subnet.main[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_main_route_table_association" "public_main" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.public.id
+}
+
